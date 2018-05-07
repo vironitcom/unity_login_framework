cda1437f5a9f31f1c4c9a13392d56e726a42fad7
y.shumovsky <y.shumovsky@vironit.co.uk>
2018-05-07 09:18:18 +0300

    Rename file

---
+++ Assets/LoginFramework/Scripts/Platforms/Interfaces/IBackendPlatform.cs
@@ -0,0 +1,23 @@
+﻿/***************************** Module Header *****************************\
+Module Name:  IBackendPlatform.cs
+Project:      Vironit Unity3D Login Framework
+Copyright (c) VironIT, http://https://vironit.com
+
+Interface for IBackend Platform
+
+The MIT License (MIT)
+
+THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+namespace VitonIT.LoginFramework
+{
+    public interface IBackendPlatform
+    {
+        LoginUser GetUser();
+        void Init();
+        void RegisterUser(string mail, string pass, string name);
+        void SignOut();
+    }
+}
